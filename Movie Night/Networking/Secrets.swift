3f1542b181b87eff6d66f8380ff706247b827249
Lukas Kasakaitis <lukas.kasakaitis@icloud.com>
2019-07-19 17:11:35 +0200

    Update secrets.

---
+++ Movie Night/Networking/Secrets.swift
@@ -0,0 +1,16 @@
+//
+//  Secrets.swift
+//  Movie Night
+//
+//  Created by Lukas Kasakaitis on 17.07.19.
+//  Copyright © 2019 Lukas Kasakaitis. All rights reserved.
+//
+
+import Foundation
+/// Object to hold application secrets
+struct Secrets {
+    
+    static let apiKey = "ENTER YOUR API KEY HERE!"
+    /// Get yours at https://www.themoviedb.org/documentation/api
+    
+}
